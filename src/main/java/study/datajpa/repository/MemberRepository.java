e3b7dc768c4f1aa488dac062b669a7a0510466d2
laveberry <laveberry7@gmail.com>
2023-12-30 17:32:45 +0900

    메소드 이름으로 쿼리 생성

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -7,6 +7,9 @@ import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     //이름으로 회원찾기 - 도메인에 특화된 기능은 공통으로 만들기 불가. 구현하지않아도 springDataJpa가 작동시킴(쿼리메소드기능)
-    List<Member> findByUsername(String username);
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    //find... : 아무거나 들어가도됨, By 이후 안넣으면 전체조회
+    List<Member> findTop3HelloBy();
 
 }
