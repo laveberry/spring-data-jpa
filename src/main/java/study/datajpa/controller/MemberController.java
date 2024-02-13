1b5f54fac7bb5496bde87ea6f2470a850076989d
laveberry <laveberry7@gmail.com>
2024-02-14 06:49:38 +0900

    WEB 확장 - 페이징과 정렬

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -2,9 +2,14 @@ package study.datajpa.controller;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -26,8 +31,21 @@ public class MemberController {
         return member.getUsername();
     }
 
+    //http://localhost:8080/members?page=1&size=3&sort=id,desc
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5, sort = "username") Pageable pageable) {
+        //페이지 1부터 시작하게 정의, 신규객체 만들어서 정의해야함
+//        PageRequest request = PageRequest.of(1, 2);
+//        Page<Member> page = memberRepository.findAll(pageable);
+//        Page<MemberDto> map = page.map(member -> new MemberDto(member.getId(), member.getUsername(), null));
+
+        return memberRepository.findAll(pageable).map(MemberDto::new);
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" +i, i));
+        }
     }
 }
